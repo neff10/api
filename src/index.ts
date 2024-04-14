1db4a80d0e96ec6a461643fbdf5d0494c5b0c45d
Sule Tunkara <st.tunkara@gmail.com>
2024-04-14 01:47:09 +0100

    Api

--- src/index.ts
+++ src/index.ts
@@ -19,89 +19,59 @@ interface User {
 }
 const USERS: User[] = [];
 
-// Create user in Stream Chat
 app.post("/register", async (req, res) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    return res.status(400).json({
-      message: "Email and password are required.",
-    });
+    return res.status(400).json({ error: "Email and password are required." });
   }
 
-  // Minlength 6
   if (password.length < 6) {
-    return res.status(400).json({
-      message: "Password must be at least 6 characters.",
-    });
+    return res
+      .status(400)
+      .json({ error: "Password must be at least 6 characters." });
   }
 
   const existingUser = USERS.find((user) => user.email === email);
-
   if (existingUser) {
-    return res.status(400).json({
-      message: "User already exists.",
-    });
+    return res.status(400).json({ error: "User already exists." });
   }
 
   try {
     const hashed_password = hashSync(password, salt);
-    // Generate random id and push to in memory users
-    const id = Math.random().toString(36).substr(2, 9);
-    const user = {
-      id,
-      email,
-      hashed_password,
-    };
+    const id = Math.random().toString(36).slice(2);
+    console.log("file: index.ts ~ line 41 ~ app.post ~ id", id);
+    const user = { id: id, email, hashed_password };
     USERS.push(user);
 
-    // Create user in Stream Chat
     await client.upsertUser({
       id,
       email,
       name: email,
     });
 
-    // Create token for user
     const token = client.createToken(id);
 
-    return res.json({
-      token,
-      user: {
-        id: user.id,
-        email: user.email,
-      },
-    });
-  } catch (e) {
-    return res.json({
-      message: "User already exists.",
-    });
+    return res.status(201).json({ token, user: { id, email } });
+  } catch (error) {
+    res.status(500).json({ error: "User already exists." });
   }
 });
 
-// Login user
 app.post("/login", async (req, res) => {
   const { email, password } = req.body;
   const user = USERS.find((user) => user.email === email);
   const hashed_password = hashSync(password, salt);
 
   if (!user || user.hashed_password !== hashed_password) {
-    return res.status(400).json({
-      message: "Invalid credentials.",
-    });
+    return res.status(401).json({ error: "Invalid credentials." });
   }
-  // Create token for user
+
   const token = client.createToken(user.id);
 
-  return res.json({
-    token,
-    user: {
-      id: user.id,
-      email: user.email,
-    },
-  });
+  return res.status(200).json({ token, user: { id: user.id, email } });
 });
 
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
